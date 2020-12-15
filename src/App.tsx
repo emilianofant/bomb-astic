237ee964951df7f6acbceb948e28aa438f230a5d
Emiliano Fant <emilianofant@github.com>
2020-12-15 10:36:40 -0300

    Added Semantic-ui and custom font

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'semantic-ui-react'
 import './App.scss';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button>Click Here</Button>
     </div>
   );
 }
