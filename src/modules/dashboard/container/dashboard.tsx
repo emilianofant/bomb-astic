3c0b02a7849908387bed2b8ec3dba930713fba8f
Emiliano Fant <emilianofant@github.com>
2020-12-15 12:13:08 -0300

    feat: Routing setup, added 3 routes

---
+++ src/modules/dashboard/container/dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Dashboard extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h2>This is the Dashboard !</h2>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
---
+++ src/modules/game/container/game.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Game extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h2>This is the Game!</h2>
+      </div>
+    );
+  }
+}
+
+export default Game;
