0cf87bd61d310b21280305d7d0fd1e1fa1f61264
Emiliano Fant <emilianofant@github.com>
2020-12-15 10:51:56 -0300

    Added eslint & prettier, removed webvitals

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Button } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react';
 import './App.scss';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
